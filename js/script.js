b8f80db1fb0d84dd11a86b0d11a2c3962abf526e
Anna Kowalczuk <anna@kowalczuk.eu>
2018-07-25 17:45:56 +0200

    Add script supporting modals

--- js/script.js
+++ js/script.js
@@ -11,7 +11,44 @@
         toggleMenu()
     });
 
-    flatpickr('.date-range input',{
+    // funkcje dotyczace modali
+    function closeModal() {
+        document.getElementById('overlay').classList.remove('show')
+    }
+
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            e.preventDefault()
+            closeModal()
+        })
+    })
+
+    document.querySelector('#overlay').addEventListener('click', function (e) {
+        if (e.target === this) {
+            closeModal()
+        }
+    })
+
+    document.addEventListener('keyup', function (e) {
+        if (e.keyCode === 27) {
+            closeModal()
+        }
+    })
+
+    function openModal(modal) {
+        document.querySelectorAll('#overlay > *').forEach(function (modal) {
+            modal.classList.remove('show')
+        })
+        document.querySelector('#overlay').classList.add('show')
+        document.querySelector(modal).classList.add('show')
+    }
+
+    document.querySelector('.exit').addEventListener('click', function () {
+        openModal('#myModal')
+    });
+
+    // obsluga kalendarza
+    flatpickr('.date-range input', {
         altInput: true,
         altFormat: 'd.m.Y',
         dateFormat: 'Y-m-d',
