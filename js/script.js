efdf2552482302cec178cc5770444bc091dfc2fb
Anna Kowalczuk <anna@kowalczuk.eu>
2018-07-29 15:35:25 +0200

    Add chart

--- js/script.js
+++ js/script.js
@@ -61,4 +61,40 @@
         dateFormat: 'Y-m-d',
     });
 
+    // generowanie wykresu
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var chart = new Chart(ctx, {
+        // typ wykresu
+        type: 'bar',
+        data: {
+            // etykiety osi X
+            labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+            // ustawienie serii danych 
+            datasets: [{
+                // nadanie im nazwy
+                label: "Signups",
+                // wybór koloru serii
+                backgroundColor: '#8DBEC8',
+                borderColor: '#8DBEC8',
+                // uzupełnienie przykładowymi danymi
+                data: [ 350, 200, 220, 390, 420, 400, 300, 290, 300 ],
+            },
+            {
+                label: "FTD",
+                backgroundColor: '#F29E4E',
+                borderColor: '#F29E4E',
+                data: [ 410, 180, 300, 290, 430, 120, 200, 500, 350 ],
+            },
+            {
+                label: "Earned",
+                backgroundColor: '#71B374',
+                borderColor: '#71B374',
+                data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48 ],
+                // ukrycie ostatniej serii danych
+                hidden: true,
+            }]
+        },
+    });
+
 })();
